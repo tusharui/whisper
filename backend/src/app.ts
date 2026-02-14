d22908834e1e9809e7463cc1f2bcd16e0f079b73
Tushar <tusharanshu18@gmail.com>
2026-02-14 13:34:01 +0530

    setup clerk auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,12 @@ import authRoutes from "./routes/authRoutes"
 import chatRoutes from "./routes/chatRoutes"
 import messageRoutes from "./routes/messageRoutes"
 import userRoutes from "./routes/userRoutes"
+import { clerkMiddleware } from '@clerk/express'
 const app = express();
 
 app.use(express.json())
+app.use(clerkMiddleware())
+
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
