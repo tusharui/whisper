caa252ff99f0cb386451589da6990880bbddb85b
Tushar <tusharanshu18@gmail.com>
2026-02-15 08:44:05 +0530

    added chatcontrollers

---
+++ backend/src/controllers/chatControllers.ts
@@ -0,0 +1,73 @@
+import type { NextFunction, Response } from "express";
+import type { AuthRequest } from "../middleware/auth";
+import { Chat } from "../models/Chat";
+
+export async function getChats(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+        const userId = req.userId;
+        const chats = await Chat.find({ participants: userId })
+            .populate("participants", "name email avatar")
+            .populate("lastMessage")
+            .sort({ lastMessageAt: -1 })
+
+        const formattedChats = chats.map(chat => {
+            const otherParticipant = chat.participants.find(p => p._id.toString()! == userId)
+
+            return {
+                _id: chat._id,
+                participant: otherParticipant ?? null,
+                lastMessage: chat.lastMessage,
+                lastMesasageAt: chat.lastMessageAt,
+                createdAt: chat.createdAt,
+            };
+        });
+
+        res.json(formattedChats)
+    } catch (error) {
+        res.status(500);
+        next(error);
+    }
+}
+export async function getOrCreateChat(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+        const userId = req.userId;
+        const { participantId } = req.params;
+
+        if (!participantId) {
+            res.status(400).json({ message: "Participant ID is required" });
+            return;
+        }
+
+        
+
+        if (userId === participantId) {
+            res.status(400).json({ message: "Cannot create chat with yourself" });
+            return;
+        }
+
+
+        let chat = await Chat.findOne({
+            participants: { $all: [userId, participantId] },
+        })
+            .populate("participants", "name email avatar")
+            .populate("lastMessage")
+        if (!chat) {
+            const newChat = new Chat({ participants: [userId, participantId] });
+            await newChat.save();
+            chat = await newChat.populate("participants", "name email avatar");
+        }
+
+        const otherParticipant = chat.participants.find((p: any) => p._id.toString() !== userId);
+        res.json({
+            _id: chat._id,
+            participant: otherParticipant ?? null,
+            lastMessage: chat.lastMessage,
+            lastMesasageAt: chat.lastMessageAt,
+            createdAt: chat.createdAt,
+        })
+    } catch (error) {
+        res.status(500);
+        next(error);
+    }
+}
+
--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,3 +1,9 @@
 import {Router} from "express";
+import { protectRoute } from "../middleware/auth";
+import {getChats , getOrCreateChat} from "../controllers/chatControllers";
 const router =Router();
+
+router.use(protectRoute);
+router.get("/", getChats)
+router.post("/with/:participantId", getOrCreateChat);
 export default router;
