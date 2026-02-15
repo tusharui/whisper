4296c5f3d8ac2b99794b647acefc6f398615d7ad
Tushar <tusharanshu18@gmail.com>
2026-02-15 06:54:23 +0530

    added usercontrollers

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,3 +1,8 @@
 import {Router} from "express";
+import { protectRoute } from "../middleware/auth";
+import { getUsers } from "../controllers/userControllers";
+
 const router =Router();
+
+router.get("/",protectRoute,getUsers);
 export default router;
