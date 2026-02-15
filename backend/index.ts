61e06a03e6a92c5d58b8c49bedbb0330e168f8d6
Tushar <tusharanshu18@gmail.com>
2026-02-15 13:29:03 +0530

    add socket.io real-time chat implementation

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,20 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
-
+import {createServer}from "http";
+import { initializeSocket } from "./src/utils/socket";
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("✅ server is running on port:", PORT);
-    });
-}).catch((error) => {
-    console.error("failed to start server:", error);
-    process.exit(1);
+const httpServer = createServer(app)
 
-});
+
+initializeSocket(httpServer)
+connectDB()
+    .then(() => {
+        httpServer.listen(PORT, () => {
+            console.log("✅ server is running on port:", PORT);
+        });
+    }).catch((error) => {
+        console.error("failed to start server:", error);
+        process.exit(1);
+
+    });
